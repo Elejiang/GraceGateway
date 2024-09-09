d256f2d54f12ab10bc9174b4d371a838521804fc
优雅 <elegance520@163.com>
2024-09-09 17:05:53 +0800

    config模块扩展路由定义

--- GraceGateway-Config/src/main/java/com/grace/gateway/config/config/RouteDefinition.java
+++ GraceGateway-Config/src/main/java/com/grace/gateway/config/config/RouteDefinition.java
@@ -3,6 +3,7 @@ package com.grace.gateway.config.config;
 import lombok.Data;
 
 import java.net.URI;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -19,5 +20,30 @@ public class RouteDefinition {
 
     // 路由顺序，当请求匹配到多个路由时，选择顺序小的
     private int order = 0;
+
+    // 超时时间，单位ms
+    private int timeout = 3000;
+
+    // 重试次数
+    private int retryTimes = 3;
+
+    // 路由需要走的过滤器
+    private Set<FilterConfig> filterConfigs;
+
+
+    @Data
+    public static class FilterConfig {
+
+        /**
+         * 过滤器名字，唯一的
+         */
+        private String name;
+
+        /**
+         * 过滤器规则描述，json
+         */
+        private String config;
+
+    }
 
 }
