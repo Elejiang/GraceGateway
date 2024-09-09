e05d465c15135243fd41a1fecb08a838dabd1fa4
优雅 <elegance520@163.com>
2024-09-09 16:56:03 +0800

    config模块新增服务定义与实例

---
+++ GraceGateway-Config/src/main/java/com/grace/gateway/config/config/ServiceDefinition.java
@@ -0,0 +1,37 @@
+package com.grace.gateway.config.config;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 服务定义
+ */
+@Data
+public class ServiceDefinition implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -3751925283314024046L;
+
+    /**
+     * 服务名
+     */
+    private String serviceName;
+
+    /**
+     * 版本号
+     */
+    private String version;
+
+    /**
+     * 环境
+     */
+    private String envType;
+
+    /**
+     * 服务是否启用
+     */
+    private boolean enable = true;
+
+}
---
+++ GraceGateway-Config/src/main/java/com/grace/gateway/config/config/ServiceInstance.java
@@ -0,0 +1,57 @@
+package com.grace.gateway.config.config;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 服务实例
+ */
+@Data
+public class ServiceInstance implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -7137947815268291319L;
+
+    /**
+     * 服务名
+     */
+    private String serviceName;
+
+    /**
+     * 实例id：ip:port
+     */
+    private String instanceId;
+
+    /**
+     * 服务实例 ip
+     */
+    private String ip;
+
+    /**
+     * 服务实例 port
+     */
+    private int port;
+
+    /**
+     * 权重信息
+     */
+    private int weight = 1;
+
+    /**
+     * 服务实例是否启用
+     */
+    private boolean enable = true;
+
+    /**
+     * 服务实例是否灰度
+     */
+    private boolean gray;
+
+    /**
+     * 服务实例灰度比例
+     */
+    private double threshold;
+
+}
